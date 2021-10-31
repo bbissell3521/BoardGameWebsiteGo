9c4ccbc734f44992942902d524934e5606c6e15a
Blake <blakejbissell@gmail.com>
2021-10-31 13:44:07 -0500

    Init Commit

---
+++ server/app.go
@@ -0,0 +1,15 @@
+package main
+
+type game struct {
+	ID          string
+	Title       string
+	PublishDate string
+	Thumbnail   string
+	Description string
+	Genre       string
+	Publisher   string
+}
+
+func accessAPI() {
+
+}
